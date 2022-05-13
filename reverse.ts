5f5449fa2dad9041a2dd564abeeb643b44a79c8c
sazix <86483949+Sazix1337@users.noreply.github.com>
2022-05-13 11:48:17 +0300

    Update reverse.ts

--- reverse.ts
+++ reverse.ts
@@ -1,9 +1,18 @@
 function reverseString(stringValue?: string): ReverseString {
-    const reverseArray: Array<string> = []
-    for (let i = 0; i < stringValue.length; i++) {
-        reverseArray.unshift(stringValue[i])
+    if (stringValue) {
+        const reverseArray: Array<string> = []
+        for (let i = 0; i < stringValue.length; i++) {
+            reverseArray.unshift(stringValue[i])
+        }
+        return reverseArray.join('')
+    } else {
+        stringValue = "Hello World"
+        const reverseArray: Array<string> = []
+        for (let i = 0; i < stringValue.length; i++) {
+            reverseArray.unshift(stringValue[i])
+        }
+        return reverseArray.join('')  
     }
-    return reverseArray.join('')
 }
 
 export default reverseString
